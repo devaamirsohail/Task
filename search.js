07715c9f0a75925dbef9c1df5238eb7ce3580b8a
devaamirsohail <dev.aamirsohail@gmail.com>
2020-02-05 11:02:30 -0500

    Search method

---
+++ search.js
@@ -0,0 +1,25 @@
+const criminals = new Map();
+
+criminals.set("Paul White", "Roger Night, Peter Llong Jr.");
+criminals.set("Roger Fedexer", "Rob Ford, Pete Lord, Roger McWire");
+criminals.set("Paul White Jr.", null);
+criminals.set("Red Fortress", "Roger Rabbit, Ross Winter");
+criminals.set("Redford Fort", "Red Strong, Red Fort");
+
+function interviewCriminals(input) {
+  //We'll consider taking the input into uppercase
+  const changedInput = input.toLowerCase();
+  for (const [key, value] of criminals.entries()) {
+    if (key.toLowerCase().includes(changedInput)) {
+      const result = criminals.get(key);
+      return `Name=${key}, Alias=${result}`;
+    } else if (value) {
+      if (value.toLowerCase().includes(changedInput)) {
+        const result = criminals.get(key);
+        return `Name=${key}, Alias=${result}`;
+      }
+    }
+  }
+}
+
+console.log(interviewCriminals("Rabbit"));
